f3b2c409f41f804885099ab328a6a39385b84a82
ghillman <ghillman@live.co.uk>
2018-12-18 17:18:13 +0000

    Commit
    Sorted out another enemy(fireblob thing) no contact with player yet and still working on fireball particals to get them lit with lighting so they look like embers

---
+++ AE3/Assets/Scenes/Enemies/Fire blob/EmberAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmberAttack : MonoBehaviour {
+
+    public Rigidbody2D EmberRigid;
+    private GameObject Fireblob;
+    public float Speed;
+    private float _Speed;
+    private bool AssignedParent;
+	// Use this for initialization
+	void Start () {
+        Fireblob = GameObject.Find("Fireblob");
+        AssignedParent = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _Speed = Speed * Time.deltaTime;
+
+        if (AssignedParent)
+        {
+            gameObject.transform.SetParent(Fireblob.transform);
+
+            if (GetComponentInParent<FireBlobMovement>().EmberSpawn() == true)
+            {
+                EmberRigid.velocity = new Vector2(_Speed, 0);
+            }
+            else if (GetComponentInParent<FireBlobMovement>().EmberSpawn() == false)
+            {
+                EmberRigid.velocity = new Vector2(-_Speed, 0);
+            }
+            AssignedParent = false;
+
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D Target)
+    {
+        if (Target.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ AE3/Assets/Scenes/Enemies/Fire blob/FireBlobMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBlobMovement : MonoBehaviour {
+
+    public Rigidbody2D BlobRigid;
+    public GameObject Ember;
+    public float Speed;
+    private float _Speed;
+    public bool LeftRight;
+    public float AgroDistance;
+    public float JumpLimit;
+    public float Spawntimer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _Speed = Speed * Time.deltaTime;
+        if (Spawntimer < 2)
+        {
+            Spawntimer += Time.deltaTime;
+        }
+        if(LeftRight)
+        {
+            
+            BlobRigid.velocity = new Vector2(_Speed, BlobRigid.velocity.y);
+            Debug.DrawRay(transform.position + new Vector3(0, 0, 0), new Vector3(AgroDistance, 0, 0), Color.red);
+            RaycastHit2D Target =
+            Physics2D.Raycast(transform.position, new Vector3(AgroDistance, 0, 0), AgroDistance, 1 << LayerMask.NameToLayer("Player"));
+            if (Target.collider != null)
+            {
+                if (Target.collider.gameObject.CompareTag("Player"))
+                {
+                    EmberSpawn();
+                }
+            }
+        }
+        else
+        {
+            BlobRigid.velocity = new Vector2(-_Speed, BlobRigid.velocity.y);
+            Debug.DrawRay(transform.position + new Vector3(0, 0, 0), new Vector3(-AgroDistance, 0, 0), Color.red);
+            RaycastHit2D Target =
+            Physics2D.Raycast(transform.position, new Vector3(-AgroDistance, 0, 0), AgroDistance, 1 << LayerMask.NameToLayer("Player"));
+            if (Target.collider != null)
+            {
+                if (Target.collider.gameObject.CompareTag("Player"))
+                {
+                    EmberSpawn();
+                }
+            }
+        }
+        
+
+        Debug.DrawLine(transform.position, transform.position + new Vector3(0, -JumpLimit, 0), Color.blue);
+       
+      
+
+
+    }
+    public bool EmberSpawn()
+    {
+        if (Spawntimer >= 2)
+        {
+            Spawntimer = 0;
+            Instantiate(Ember, transform.position, Quaternion.identity);
+            return LeftRight;
+        }
+        return LeftRight;
+    }
+    private void OnTriggerEnter2D(Collider2D Target)
+    {
+        if(Target.gameObject.CompareTag("Node"))
+        {
+            if(LeftRight)
+            {
+                LeftRight = false;
+            }
+            else if(!LeftRight) 
+            {
+                LeftRight = true;
+            }
+        }
+    }
+}
