78dbcc6c297ec4df4c2132275b7322999c66d42d
ghillman <ghillman@live.co.uk>
2019-01-07 17:12:48 +0000

    Started building a few levels
    up to level 7 roughly made

---
+++ AE3/Assets/Scenes/Scripts/FlamePlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamePlatform : MonoBehaviour {
+
+    public float Flame;
+
+	// Use this for initialization
+	void Start () {
+        
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Flame += Time.deltaTime;
+        if(Flame >= 1)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<BoxCollider2D>().enabled = true;
+            if(Flame >= 2)
+            {
+                Flame = 0;
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+	}
+}
