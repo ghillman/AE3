65053a190ecae125abef61d73ad9bd735a637833
ghillman <ghillman@live.co.uk>
2019-01-11 17:07:57 +0000

    level 13 complete
    Still need assets for the enemies and for player animations to be finished

---
+++ AE3/Assets/Scenes/Scripts/Graham's scripts/LevelThirteenDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelThirteenDoor : MonoBehaviour {
+
+    public EdgeCollider2D Door;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GetComponent<AttackScript>().Alive == false)
+        {
+            Door.enabled = false;
+        }
+	}
+}
