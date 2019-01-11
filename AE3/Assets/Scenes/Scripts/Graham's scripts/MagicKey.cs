01ee613b733070e5d68eeb4fa19c4ca7e25339ff
ghillman <ghillman@live.co.uk>
2019-01-11 16:29:50 +0000

    more stuff done

---
+++ AE3/Assets/Scenes/Scripts/Graham's scripts/MagicKey.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicKey : MonoBehaviour {
+    public EdgeCollider2D Door;
+	
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Door.enabled = false;
+        Destroy(gameObject);
+    }
+}
