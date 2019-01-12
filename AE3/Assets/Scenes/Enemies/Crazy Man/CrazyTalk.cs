9981b1da28ef29175dbb2daa8103a653ef288816
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-01-12 00:12:32 +0000

    Few More Scenes Drawn + LV23 NPC
    Wanted to do more, but had to pack to come down tomorrow, and have to go to bed earlier so I can get up and moved down.

---
+++ AE3/Assets/Scenes/Enemies/Crazy Man/CrazyTalk.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrazyTalk : MonoBehaviour
+{
+    public Text ItTalks;
+    public float TalkSpeed;
+    private string introText = "Can you see them traveller?";
+    private string nextIntroText = "They are watching us.";
+    private string finalIntroText = "They are CONTROLLING you";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //start talking animation
+        StartCoroutine(Introduction());
+        GetComponent<Animator>().SetBool("Visited", true);
+        StartCoroutine(Introduction());
+    }
+
+    private IEnumerator Introduction()
+    {
+        for (int i = 0; i < introText.Length; i++)
+        {
+            //cycles through string 1 character at a time
+            ItTalks.text = introText.Substring(0, i);
+            yield return new WaitForSeconds(TalkSpeed);
+        }
+        for (int i = 0; i < nextIntroText.Length; i++)
+        {
+            ItTalks.text = introText.Substring(0, i);
+            yield return new WaitForSeconds(TalkSpeed);
+        }
+        for (int i = 0; i < finalIntroText.Length; i++)
+        {
+            ItTalks.text = introText.Substring(0, i);
+            yield return new WaitForSeconds(TalkSpeed);
+        }
+    }
+}
