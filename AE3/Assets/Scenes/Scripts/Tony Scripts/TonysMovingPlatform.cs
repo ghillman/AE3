9e033b444ad229b7f08aff58fc39ff8cb6e2d18b
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-01-14 15:14:30 +0000

    Level 24

---
+++ AE3/Assets/Scenes/Scripts/Tony Scripts/TonysMovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TonysMovingPlatform : MonoBehaviour
+{
+    public float MoveSpeed;
+    private bool MovingRight = true;
+
+
+    //used for physics operations
+    private void FixedUpdate()
+    {
+        if (MovingRight)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);
+        else
+            GetComponent<Rigidbody2D>().velocity = new Vector2((MoveSpeed * Time.deltaTime) * -1, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    //If platform hits
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (MovingRight)
+            MovingRight = false;
+        else
+            MovingRight = true;
+    }
+
+}
