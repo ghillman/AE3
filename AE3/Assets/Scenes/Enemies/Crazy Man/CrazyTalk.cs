dca454406425ef2bb7113bcbd7310a04229371db
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-01-14 15:10:01 +0000

    Level 23, Button Manager Fixes

--- AE3/Assets/Scenes/Enemies/Crazy Man/CrazyTalk.cs
+++ AE3/Assets/Scenes/Enemies/Crazy Man/CrazyTalk.cs
@@ -5,37 +5,104 @@ using UnityEngine.UI;
 
 public class CrazyTalk : MonoBehaviour
 {
+    //public Crazy
     public Text ItTalks;
-    public float TalkSpeed;
-    private string introText = "Can you see them traveller?";
-    private string nextIntroText = "They are watching us.";
-    private string finalIntroText = "They are CONTROLLING you";
+    public float SpeechDelay;
+    //Dialogue
+    private string Intro = "Can you see them traveller?";
+    private bool ReadText = false;
 
+    //Responses
+    [HideInInspector]
+    public bool Responded = false;
+    [HideInInspector]
+    public bool Answer;
+
+    //Responses to choice
+    public Button Yes;
+    private string TheyKnow = "The memories of your \n fatalities must be painful.";
+    public Button No;
+    private string TheyAreButAPuppet = "Ah yes, \n you only look left and right.";
+
+    //Final Line
+    private bool HeRepeats = false;
+    private string HeWatches = "They see a man in chains \n but it is I who is looking \n at someone who is not free";
+
+    //Player triggers Crazy Person
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //start talking animation
-        StartCoroutine(Introduction());
         GetComponent<Animator>().SetBool("Visited", true);
-        StartCoroutine(Introduction());
+
+        //start talking
+        if (!HeRepeats)
+            StartCoroutine(Dialogue());
+        //keep saying this when triggered
+        else
+            StartCoroutine(RepeatedLine());
     }
 
-    private IEnumerator Introduction()
+    private IEnumerator Dialogue()
     {
-        for (int i = 0; i < introText.Length; i++)
+        //Skip this if intro happened
+        if (!ReadText)
+        {
+            for (int i = 0; i < Intro.Length + 1; i++)
+            {
+                //cycles through string 1 character at a time
+                ItTalks.text = Intro.Substring(0, i);
+                //delay next character
+                yield return new WaitForSeconds(SpeechDelay);
+            }
+            ReadText = true;
+            Yes.gameObject.SetActive(true);
+            No.gameObject.SetActive(true);
+
+        }
+        if (Responded)
+        {
+            //clear speech
+            ItTalks.text = "";
+
+            //answered Yes
+            if (Answer)
+            {
+                for (int i = 0; i < TheyKnow.Length + 1; i++)
+                {
+                    //cycles through string 1 character at a time
+                    ItTalks.text = TheyKnow.Substring(0, i);
+                    //delay next character
+                    yield return new WaitForSeconds(SpeechDelay);
+                }
+            }
+            if (!Answer)
+            {
+                for (int i = 0; i < TheyAreButAPuppet.Length + 1; i++)
+                {
+                    //cycle through string 1 character at a time
+                    ItTalks.text = TheyAreButAPuppet.Substring(0, i);
+                    //delay next character
+                    yield return new WaitForSeconds(SpeechDelay);
+                }
+            }
+            HeRepeats = true;
+        }
+        //stop animation
+        GetComponent<Animator>().SetBool("Visited", false);
+        yield return new WaitForSeconds(SpeechDelay);
+    }
+    private IEnumerator RepeatedLine()
+    {
+        //clear text
+        ItTalks.text = "";
+        for (int i = 0; i < HeWatches.Length + 1; i++)
         {
             //cycles through string 1 character at a time
-            ItTalks.text = introText.Substring(0, i);
-            yield return new WaitForSeconds(TalkSpeed);
+            ItTalks.text = HeWatches.Substring(0, i);
+            //delay next character
+            yield return new WaitForSeconds(SpeechDelay);
         }
-        for (int i = 0; i < nextIntroText.Length; i++)
-        {
-            ItTalks.text = introText.Substring(0, i);
-            yield return new WaitForSeconds(TalkSpeed);
-        }
-        for (int i = 0; i < finalIntroText.Length; i++)
-        {
-            ItTalks.text = introText.Substring(0, i);
-            yield return new WaitForSeconds(TalkSpeed);
-        }
+        //stop animation
+        GetComponent<Animator>().SetBool("Visited", false);
     }
 }
--- AE3/Assets/Scenes/Scripts/Tony Scripts/ButtonManager.cs
+++ AE3/Assets/Scenes/Scripts/Tony Scripts/ButtonManager.cs
@@ -5,105 +5,38 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    public float RayCastDown;
-    public bool LeftButton;
-    public bool RightButton;
-    private Animator Animated;
-    private bool MovingLeft = false;
-    private bool MovingRight = false;
-    private bool stopleft;
-    private bool stopright;
-    private bool waitBool;
-    private float WaitTime;
-    public Rigidbody2D PlayerRigid;
-    public float MoveSpeed;
-    void Moveleft()
-    {
-        if (LeftButton)
-        {
-            if (MovingLeft)
-            {
-                //move the player
-                if (PlayerRigid.gameObject.GetComponent<PlayerMovement>().Hit == false)
-                {
-                    PlayerRigid.velocity = new Vector2((MoveSpeed * Time.deltaTime)* -1, PlayerRigid.velocity.y);
-                    Debug.Log("Hello");
-                    Debug.Log(MoveSpeed);
-                    stopleft = true;
-                }
-            }
-            else if(stopleft && !MovingLeft)
-            {
-                PlayerRigid.velocity = new Vector2(0, PlayerRigid.velocity.y);
-                
-                Debug.Log("good bye");
-                stopleft = false;
-                GetComponent<Image>().color = new Color(1, 1, 1, 0.66f);
-            }
-        }
-    }
-    void MoveRight()
-    {
-        if (RightButton)
-        {
-            if (MovingRight)
-            {
-                if (PlayerRigid.gameObject.GetComponent<PlayerMovement>().Hit == false)
-                {
-                    //move the player
-                    PlayerRigid.velocity = new Vector2((MoveSpeed * Time.deltaTime) * 1, PlayerRigid.velocity.y);
-                    Debug.Log(MoveSpeed);
-                    stopright = true;
-                }
-            }
-            else if (stopright && !MovingRight)
-            {
-                PlayerRigid.velocity = new Vector2(0, PlayerRigid.velocity.y);
-                stopright = false;
-                GetComponent<Image>().color = new Color(255, 255, 255, 0.66f);
-            }
-        }
-    }
 
-    private void Update()
-    {
-        if (waitBool)
-        {
-            WaitTime += Time.deltaTime;
-            if (WaitTime >= 0.2f)
-            {
-                waitBool = false;
-                WaitTime = 0;
-                GetComponent<Image>().color = new Color(1, 1, 1, 0.66f);
-            }
-        }
+    //----------------------------------------------------------------MOVEMENT-----------------------------------------------------------------------------------//
 
-        Moveleft();
-        MoveRight();
-        Debug.DrawLine(PlayerRigid.transform.position, PlayerRigid.transform.position + new Vector3(0, RayCastDown, 0), Color.blue);
-        
-    }
+    //JUMPING
+    private float RayCastDown = -0.8f;
     public void Jump(Rigidbody2D Player)
     {
-        //draw raycast
+        //Highlight button
         GetComponent<Image>().color = new Color(255, 255, 255, 255);
-        waitBool = true;
+        //draw raycast
+        Debug.DrawLine(Player.transform.position, Player.transform.position + new Vector3(0, RayCastDown, 0), Color.blue);
         //if raycast hits ground allow player to jump
         if (Physics2D.Linecast(Player.transform.position, Player.transform.position + new Vector3(0, RayCastDown, 0), 1 << LayerMask.NameToLayer("Ground")))
         {
-            
+
             Player.velocity = new Vector2(Player.velocity.x, FindObjectOfType<PlayerMovement>().JumpHeight * Time.deltaTime);
             //play animations
             FindObjectOfType<PlayerMovement>().JumpParticals.SetActive(false);
             FindObjectOfType<PlayerMovement>().JumpParticals.SetActive(true);
         }
-        
+
     }
 
+
+    //LEFT
+    private bool MovingLeft = false;
+    //When button is held down
     public void Left(PlayerMovement Player)
     {
         if (!MovingLeft)
         {
+            //highlight button
             GetComponent<Image>().color = new Color(255, 255, 255, 255);
             //play animation
             Player.Animated.SetFloat("Moving", 1);
@@ -111,23 +44,31 @@ public class ButtonManager : MonoBehaviour
             Player.GetComponent<SpriteRenderer>().flipX = true;
             Player.Balls.GetComponent<SpriteRenderer>().flipX = true;
             Player.Eyes.GetComponent<SpriteRenderer>().flipX = true;
-            //move Eyes
+            //re-locate Eyes
             Player.Eyes.transform.position = new Vector2(Player.transform.position.x - 0.16f, Player.transform.position.y + 0.43f);
             //Rotates origins of attacks
             Player.BigAttack.transform.rotation = Quaternion.Euler(0, -90, 90);
             FindObjectOfType<LightningAttack>().transform.localScale = new Vector2(-2, 2);
-
             MovingLeft = true;
         }
-        else
-        {
-            Player.Animated.SetFloat("Moving", 0);
-            MovingLeft = false;
-            
-        }
-
+        //move player
+        Player.PlayerRigid.velocity = new Vector2((Player.MoveSpeed * Time.deltaTime) * -1, Player.PlayerRigid.velocity.y);
     }
 
+    //When left button is released
+    public void LeftUp(PlayerMovement Player)
+    {
+        //take off highlight
+        GetComponent<Image>().color = new Color(1, 1, 1, 0.66f);
+        //stop player
+        Player.PlayerRigid.velocity = new Vector2(0, Player.PlayerRigid.velocity.y);
+        //stop animation
+        Player.Animated.SetFloat("Moving", 0);
+        MovingLeft = false;
+    }
+
+    //RIGHT
+    private bool MovingRight = false;
     public void Right(PlayerMovement Player)
     {
         if (!MovingRight)
@@ -139,43 +80,52 @@ public class ButtonManager : MonoBehaviour
             Player.GetComponent<SpriteRenderer>().flipX = false;
             Player.Balls.GetComponent<SpriteRenderer>().flipX = false;
             Player.Eyes.GetComponent<SpriteRenderer>().flipX = false;
-            //move the eyes
+            //re-locate eyes
             Player.Eyes.transform.position = new Vector2(Player.transform.position.x + 0.16f, Player.transform.position.y + 0.43f);
             //Rotates origins of attacks
             Player.BigAttack.transform.rotation = Quaternion.Euler(0, 90, 90);
             FindObjectOfType<LightningAttack>().transform.localScale = new Vector2(2, 2);
-
             MovingRight = true;
         }
-        else
-        {
-            Player.Animated.SetFloat("Moving", 0);
-            MovingRight = false;
-        }
+        Player.PlayerRigid.velocity = new Vector2((Player.MoveSpeed * Time.deltaTime) * 1, Player.PlayerRigid.velocity.y);
 
     }
 
+    //When Right button is released
+    public void Rightup(PlayerMovement Player)
+    {
+        //take off highlight
+        GetComponent<Image>().color = new Color(1, 1, 1, 0.66f);
+        //stop player
+        Player.PlayerRigid.velocity = new Vector2(0, Player.PlayerRigid.velocity.y);
+        //stop animation
+        Player.Animated.SetFloat("Moving", 0);
+        MovingRight = false;
+    }
+    //-----------------------------------------------------------ATTACKS------------------------------------------------------------------------------//
+
     public void Lightning()
     {
+        //Highlight button
         GetComponent<Image>().color = new Color(255, 255, 255, 255);
-        waitBool = true;
         if (FindObjectOfType<LightningAttack>().Strike == false)
         {
             FindObjectOfType<LightningAttack>().Strike = true;
+            //display particle effect
             FindObjectOfType<LightningAttack>().Particle.SetActive(true);
+            //sound effect
             FindObjectOfType<AudioManager>().Play("Lightning");
-        }
-
-        if (FindObjectOfType<LightningAttack>().Strike)
-        {
+            //get the recharge time     
             FindObjectOfType<LightningAttack>().Rechargetime += Time.deltaTime;
+            //recharge time greater than 1
             if (FindObjectOfType<LightningAttack>().Rechargetime >= 1)
             {
+                //display lightning, give it a hit box, and play its' animation
                 FindObjectOfType<LightningAttack>().GetComponent<SpriteRenderer>().enabled = true;
                 FindObjectOfType<LightningAttack>().GetComponent<BoxCollider2D>().enabled = true;
-
                 FindObjectOfType<LightningAttack>().GetComponent<Animator>().SetBool("Lightning", true);
 
+                //At the end of the attack, disable everything to do with the lightning: Sprites, hitbox, etc
                 FindObjectOfType<LightningAttack>().LightningTime += Time.deltaTime;
                 if (FindObjectOfType<LightningAttack>().LightningTime >= 0.5f)
                 {
@@ -188,23 +138,51 @@ public class ButtonManager : MonoBehaviour
                     FindObjectOfType<LightningAttack>().Rechargetime = 0;
                 }
             }
+
         }
     }
 
-    public void BigAttack(PlayerMovement Player)
+
+    //WE WANT THIS TO BE A SWIPE NOT A BUTTON, IT DOESN'T BELONG HERE
+    //public void BigAttack(PlayerMovement Player)
+    //{
+    //    //displays attack and then disperses it
+    //    if (Player.BigAttackBool == false)
+    //    {
+    //        //set bool to true, twice for some reason
+    //        Player.BigAttackBool = true;
+    //        Player.BigAttack.SetActive(true);
+    //        //make a copy transform of the collidier on the child of big attack
+    //        Transform BigAttackCollider = Player.BigAttack.transform.GetChild(0).transform;
+    //        //make attack size 0
+    //        Player.BigAttackSize = 0;
+    //        //give copy a collider
+    //        BigAttackCollider.localScale = new Vector2(Player.BigAttackSize, Player.BigAttackSize);
+    //    }
+    //}
+
+    //-------------------------------------------------------------CHOICES----------------------------------//
+
+    //player chooses yes
+    public void PicksYes()
     {
-        //displays attack and then disperses it
-        if (Player.BigAttackBool == false)
-        {
-            //set bool to true, twice for some reason
-            Player.BigAttackBool = true;
-            Player.BigAttack.SetActive(true);
-            //make a copy transform of the collidier on the child of big attack
-            Transform BigAttackCollider = Player.BigAttack.transform.GetChild(0).transform;
-            //make attack size 0
-            Player.BigAttackSize = 0;
-            //give copy a collider
-            BigAttackCollider.localScale = new Vector2(Player.BigAttackSize, Player.BigAttackSize);
-        }
+        FindObjectOfType<CrazyTalk>().Responded = true;
+        //get rid of buttons
+        FindObjectOfType<CrazyTalk>().Yes.gameObject.SetActive(false);
+        FindObjectOfType<CrazyTalk>().No.gameObject.SetActive(false);
+        //Store that player selected yes
+        FindObjectOfType<CrazyTalk>().Answer = true;
     }
+
+    //player chooses No
+    public void PicksNo()
+    {
+        FindObjectOfType<CrazyTalk>().Responded = true;
+        //get rid of buttons
+        FindObjectOfType<CrazyTalk>().Yes.gameObject.SetActive(false);
+        FindObjectOfType<CrazyTalk>().No.gameObject.SetActive(false);
+        //Store that player selected No
+        FindObjectOfType<CrazyTalk>().Answer = false;
+    }
+
 }
