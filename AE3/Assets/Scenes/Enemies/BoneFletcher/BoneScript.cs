3ace4912e219c3f8caef60239809583cd60e4570
ghillman <ghillman@live.co.uk>
2019-01-14 20:48:13 +0000

    stuff done

---
+++ AE3/Assets/Scenes/Enemies/BoneFletcher/BoneScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneScript : MonoBehaviour {
+
+    public float Speed;
+    private float _Speed;
+    [HideInInspector]
+    public bool Direction;
+    public float angleSpeed;
+    private float _Angle;
+    
+	// Use this for initialization
+	void Start () {
+        
+        _Speed = Speed * Time.deltaTime;
+        
+        if (Direction)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-_Speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            angleSpeed = angleSpeed * -1;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(_Speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _Angle += angleSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(0, 0, _Angle);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ AE3/Assets/Scenes/Enemies/BoneFletcher/BponeFletcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BponeFletcher : MonoBehaviour {
+    public GameObject Bone;
+    public bool FacingLeft;
+    private float _FireRate;
+    public float FireRate;
+
+	// Use this for initialization
+    void Start()
+    {
+        
+    }
+	
+	// Update is called once per frame]
+
+	void Update () {
+
+        _FireRate += Time.deltaTime;
+        if (_FireRate >= FireRate)
+        {
+            if (FacingLeft)
+            {
+                Bone.GetComponent<BoneScript>().Direction = true;
+            }
+            else
+            {
+                Bone.GetComponent<BoneScript>().Direction = false;
+            }
+            Instantiate(Bone, transform.position, Quaternion.identity);
+            GameObject _Bone = Instantiate(Bone, transform.position, Quaternion.identity);
+           _Bone.transform.parent = gameObject.transform;
+            _FireRate = 0;
+
+        }
+
+	}
+}
