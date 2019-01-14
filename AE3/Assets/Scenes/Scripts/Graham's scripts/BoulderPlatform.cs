9628f894cbfb849f0f6bbfd5b3772b9ecc06013e
ghillman <ghillman@live.co.uk>
2019-01-14 21:25:31 +0000

    ds

---
+++ AE3/Assets/Scenes/Scripts/Graham's scripts/BoulderPlatform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderPlatform : MonoBehaviour {
+
+    // Use this for initialization
+    public bool Fall; 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Fall = true;
+    }
+}
---
+++ AE3/Assets/Scenes/Scripts/Graham's scripts/FallingBoulders.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBoulders : MonoBehaviour {
+    public GameObject Platform;
+    public float FallTimer;
+    private float _FallTimer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        _FallTimer += Time.deltaTime;
+        if (_FallTimer >= FallTimer)
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
