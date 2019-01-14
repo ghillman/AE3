1b9ea75836486a8858fefac6475b62daff2163c9
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-01-14 15:47:15 +0000

    Level 30

---
+++ AE3/Assets/Scenes/Scripts/Tony Scripts/Switch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Touch this put a thing in the rigid body so that when the thing is touched to other thing dissapears
+public class Switch : MonoBehaviour
+{
+    public Rigidbody2D door;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        door.gameObject.SetActive(false);
+    }
+
+}
