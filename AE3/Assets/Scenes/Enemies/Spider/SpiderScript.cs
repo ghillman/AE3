6c73c6426763badce7f9bae380575a423a70efbf
ghillman <ghillman@live.co.uk>
2019-01-16 13:20:14 +0000

    Spider level done

---
+++ AE3/Assets/Scenes/Enemies/Spider/SpiderScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderScript : MonoBehaviour {
+    public Vector2 _Cooldown;
+    public GameObject Up;
+    public GameObject Middle;
+    public GameObject Down;
+    public  float Cooldown;
+    private bool GenerateCooldown;
+	// Use this for initialization
+	void Start () {
+        Cooldown = Random.Range(_Cooldown.x, _Cooldown.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GenerateCooldown)
+        {
+            Cooldown = Random.Range(_Cooldown.x, _Cooldown.y);
+            GenerateCooldown = false;
+        }
+        if (Cooldown >= 0)
+        {
+            Cooldown -= Time.deltaTime;
+        }
+        if(Cooldown <= 0)
+        {
+            GenerateCooldown = true;
+            Instantiate(Up, transform.position, Quaternion.identity);
+            Instantiate(Down, transform.position, Quaternion.identity);
+            Instantiate(Middle, transform.position, Quaternion.identity);
+        }
+
+    }
+
+}
---
+++ AE3/Assets/Scenes/Enemies/Spider/upwardDart.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upwardDart : MonoBehaviour {
+    public float MoveSpeed;
+    private float _MoveSpeed;
+    private Rigidbody2D Rigid;
+    private Vector2 MoveVector;
+    public float _Lifetime;
+    
+	// Use this for initialization
+	void Start () {
+        Rigid = GetComponent<Rigidbody2D>();
+        _MoveSpeed = MoveSpeed * Time.deltaTime;
+        MoveVector = new Vector2(-_MoveSpeed / 2, _MoveSpeed / 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _Lifetime -= Time.deltaTime;
+        if (_Lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        Rigid.velocity = MoveVector;
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
