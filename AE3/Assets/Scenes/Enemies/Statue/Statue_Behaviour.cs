fd640162dfbc9443cb1ae1b670e87743300daa9f
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-01-16 17:34:28 +0000

    Level 35

---
+++ AE3/Assets/Scenes/Enemies/Statue/Statue_Behaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statue_Behaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public bool Hit = false;
+    [HideInInspector]
+    public bool IsPossessed = false;
+    public GameObject Demon; //The glowing eyes prefab
+    private bool DemonRevealed = false;
+
+    private float delay = 5;
+    private float countdown;
+
+    public Transform Raystart;
+    public float Raycast;
+
+
+    private void Start()
+    {
+        if (IsPossessed)
+        {
+            countdown = delay;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fire Raycast
+        RaycastHit2D Hitinfo = Physics2D.Raycast(Raystart.transform.position, Raystart.transform.right,Raycast);
+        Debug.DrawLine(Raystart.transform.position, Raystart.transform.position + new Vector3(Raycast, 0), Color.red);
+        
+        if(Hitinfo)
+        {
+            //check if raycast hits warlock
+            if(Hitinfo.transform.name.Equals("Warlock"))
+            {
+                Debug.Log(Hitinfo.transform.name);
+                //check if lightning is struck near the statue
+                if (FindObjectOfType<LightningAttack>().Strike == true)
+                    Hit = true;
+                
+                //if the demon is possessed, after 5 seconds instantiate the prefab with the glowing eyes
+                if(IsPossessed && !DemonRevealed)
+                {
+                    countdown -= Time.deltaTime;
+                    if (countdown < 0)
+                    {
+                        Demon.transform.position = transform.position;
+                        Instantiate(Demon);
+                        DemonRevealed = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AE3/Assets/Scenes/Scripts/Tony Scripts/Statue_LevelManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Statue_LevelManager : MonoBehaviour
+{
+    //get statues
+    public Statue_Behaviour[] Statues; 
+    private int possessed;
+
+    //get portal
+    public GameObject Portal;
+    public Transform SpawnPortal;
+    private bool IsPortal = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //possess a random statue, tell that statue it's been possessed
+        possessed = Random.Range(0, Statues.Length);
+        Statues[possessed].IsPossessed = true;
+        Debug.Log("Statue" + possessed + "Is Possessed");
+	}
+
+    private void Update()
+    {
+        //go through all statues and check if they're hit
+        for(int i = 0; i < Statues.Length; i++)
+        {
+            //if an unpossessed statue is hit, kill the player
+            if(Statues[i].Hit && Statues[i].IsPossessed == false)
+            {
+                //kill Player/lose a life
+                //restart level
+                Debug.Log("Dead");
+                Statues[i].Hit = false;
+            }
+            //if possessed statue is hit, get rid of all statues and open the portal
+            if (Statues[possessed].Hit)
+            {
+                for (int j = 0; j < Statues.Length; j++)
+                {
+                    Statues[j].gameObject.SetActive(false);
+                }
+                if (!IsPortal)
+                {
+                    Portal.transform.position = SpawnPortal.transform.position;
+                    Instantiate(Portal);
+                    IsPortal = true;
+                }
+            }
+        }
+        
+    }
+
+}
