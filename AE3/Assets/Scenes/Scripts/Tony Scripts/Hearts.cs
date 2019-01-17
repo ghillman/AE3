1bd2a31172cfe8bcd34ee45ef2ae305a71b3d7a5
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-01-17 13:43:49 +0000

    Hearts work, damage is weird

---
+++ AE3/Assets/Scenes/Scripts/Tony Scripts/Hearts.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hearts : MonoBehaviour
+{
+    //get heart objects
+    public GameObject[] HeartObjects;
+
+    private void Update()
+    {
+        //get rid of hearts as player takes damage, reset the scene if the player loses all of them
+        switch(PlayerState.PlayerHealth)
+        {
+            case 4:
+                Debug.Log("All Hearts");
+                break;
+            case 3:
+                Debug.Log("1 Heart gone");
+                HeartObjects[3].gameObject.SetActive(false);
+                break;
+            case 2:
+                HeartObjects[2].gameObject.SetActive(false);
+                break;
+            case 1:
+                HeartObjects[1].gameObject.SetActive(false);
+                break;
+            case 0:
+                HeartObjects[0].gameObject.SetActive(false);
+                break;
+
+
+        }
+    }
+}
