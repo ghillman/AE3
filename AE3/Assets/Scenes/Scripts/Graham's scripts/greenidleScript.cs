5d978dc4999da8860cce499b3d9e9b3324759268
ghillman <ghillman@live.co.uk>
2019-01-17 13:12:44 +0000

    stuff

---
+++ AE3/Assets/Scenes/Scripts/Graham's scripts/greenidleScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class greenidleScript : MonoBehaviour {
+
+    public int NumberOfGhosts;
+    private GameObject[] GhostArray;
+    private bool Hit;
+    public Sprite Dead;
+	// Use this for initialization
+	void Start () {
+        Hit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Hit)
+        {
+            GhostArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject Ghost in GhostArray)
+            {
+                Ghost.SetActive(false);
+
+            }
+        }
+
+
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Lightning"))
+        {
+            Hit = true;
+            GetComponent<SpriteRenderer>().sprite = Dead;
+        }
+    }
+}
